c28276ae22a0eeb4b16ee7e669992d5858ef2f59
Aryan Tamang <aryantamangcs@gmail.com>
2026-07-09 09:13:24 +0545

    feat : more robust onboarding in frontend

--- apps/frontend/src/modules/auth/views/onboarding-view.tsx
+++ apps/frontend/src/modules/auth/views/onboarding-view.tsx
@@ -1,7 +1,15 @@
 "use client";
 
 import { OnboardingSection } from "@/modules/auth/components";
+import { useMe } from "../api/hooks";
+import { redirect } from "next/navigation";
+import { ROUTES } from "@/core/config";
 
 export default function OnboardingView() {
+  const { data } = useMe();
+  const user = data?.data?.user;
+  if (user && user.is_onboarded) {
+    redirect(ROUTES.DASHBOARD.ROOT);
+  }
   return <OnboardingSection />;
 }
