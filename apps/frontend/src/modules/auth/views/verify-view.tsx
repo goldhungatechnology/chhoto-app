82695470ec4da10b25252f612f9bf532be04fcd7
Aryan Tamang <aryantamangcs@gmail.com>
2026-07-09 09:15:19 +0545

    feat : more robust email verification in frontend

--- apps/frontend/src/modules/auth/views/verify-view.tsx
+++ apps/frontend/src/modules/auth/views/verify-view.tsx
@@ -1,7 +1,16 @@
 "use client";
 
 import { VerifyForm } from "@/modules/auth/components";
+import { useMe } from "../api/hooks";
+import { redirect } from "next/navigation";
+import { ROUTES } from "@/core/config";
 
 export default function VerifyView() {
+  const { data } = useMe();
+  const user = data?.data?.user;
+  if (user && user.is_email_verified) {
+    redirect(ROUTES.DASHBOARD.ROOT);
+  }
+
   return <VerifyForm />;
 }
