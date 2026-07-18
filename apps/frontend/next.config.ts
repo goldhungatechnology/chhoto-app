b5c8657a7085d0d0fcd5ea4fbacd67d35917eac7
Aryan Tamang <aryantamangcs@gmail.com>
2026-07-19 00:19:43 +0545

    feat : added devorigins

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -3,6 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  allowedDevOrigins: [
+    "http://localhost:3000",
+    "http://localhost:3001",
+    "https://auth.chhoto.tech",
+    "https://chhoto.tech",
+    "https://app.chhoto.tech",
+  ],
 };
 
 export default nextConfig;
